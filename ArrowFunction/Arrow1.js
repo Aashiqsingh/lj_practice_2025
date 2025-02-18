6b7205ae780f1caa8616fdcb1478a6f74eaf18ad
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-02-18 16:34:41 +0530

    arrow

---
+++ ArrowFunction/Arrow1.js
@@ -0,0 +1,51 @@
+// function demo()
+// {
+//     console.log("hello world");
+    
+// }
+
+// const demo = ()=>{
+//     console.log("hello world");
+    
+// }
+
+// demo();
+// const pi = 3.14;
+
+
+// function isEven(x){
+
+//     if(x % 2 == 0)
+//     {
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// var x = isEven(8)
+// console.log(x);
+
+
+// const isEven = (x)=>{
+//     return x % 2 == 0 ? "Even" : "odd"
+// }
+
+
+// function isOdd(x){
+//     return x % 2!=0;
+// }
+
+const isEven = (x)=> x % 2 == 0; 
+
+
+
+
+var x = isEven(18)
+// console.log(x == true ? "even" : "odd");
+// console.log(x == true ? "even" : "odd");
+console.log(x ?"even":"odd");
+
+
+
--- function/function2.js
+++ function/function2.js
@@ -1,6 +1,19 @@
-function pallindromeName (name){
-    return name.split("").reverse().join("")
-}
+// function pallindromeName (name){
+//     return name.split("").reverse().join("")
+// }
 
-var x = pallindromeName("palak")
-console.log(x);
+// var x = pallindromeName("palak")
+// console.log(x);
+
+// let name="my name is deep";
+// let count=0;
+// console.log(name.split(""));
+
+// for(let i=0;i<name.split("").length;i++)
+//     {
+//         if(name[i].includes('e' , 'i')){
+//             count++;
+//         }
+        
+//     }
+//     console.log(count);
