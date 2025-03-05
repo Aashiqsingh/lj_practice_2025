ce344f6a88e6e4cbcd2e81e10995ba4cf491e080
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-05 16:48:14 +0530

    foreach

---
+++ Array/array1.js
@@ -0,0 +1,73 @@
+// collection of only data 
+
+// var arr = ["ram",true,12,67457.3276]
+
+var arr = ["ram","krishna","seeta","geeta","mohan","sudama","hanuman"];
+
+// var x = arr.reverse()
+// console.log(x);
+
+// var x = arr.slice(1)
+// var x = arr.slice(1,4)
+// var x = arr.slice(-1)
+// var x = arr.slice(-4 , -1)
+// console.log(x);
+
+
+// var x = arr.sort()
+// console.log(x);
+
+// arr.splice(3,0,"rahul")
+// arr.splice(3,2,"rahul")
+// arr.splice(3,1)
+arr.splice(3,2,"rahul","tejas","samir")
+
+
+console.log(arr);
+
+
+
+
+
+
+// var x = arr.includes("seeta")
+// console.log(x);
+
+// var x = arr.indexOf("seeta")
+// console.log(x);
+
+// var x = arr.join("_")
+// var x = arr.join("$")
+// var x = arr.join(" ")
+// var x = arr.join("")
+
+// console.log(x);
+
+
+
+
+
+// console.log(arr);
+// console.log(arr.length);
+
+// var x = arr.push("tejas")
+// var x = arr.push("tejas","samir","rahul")
+
+// console.log(x);
+// console.log(arr);
+
+// var x = arr.pop()
+// console.log(x);
+
+// arr.unshift("samir")
+
+// console.log(arr);
+
+// arr.shift()
+// arr.shift()
+
+// console.log(arr);
+
+
+
+
---
+++ Array/array2.js
@@ -0,0 +1,36 @@
+var array = ["ram","krishna","seeta","geeta","mohan","sudama","hanuman"];
+
+console.log(array);
+
+// console.log(arr[0]);
+// console.log(arr[1]);
+
+// for(let i=0;i<arr.length;i++)
+// {
+//     console.log(arr[i].toUpperCase());
+    
+// }
+
+// forEach  : void 
+
+// array.forEach((arr)=>{
+//     console.log(arr);
+    
+// })
+
+// for(let i=0;i<array.length;i++)
+// {
+//     if(array[i].length > 6)
+//     {
+//         console.log(array[i]);
+        
+//     }
+// }
+
+// array.forEach((arr)=>{
+//     if(arr.length > 6)
+//     {
+//         console.log(arr);
+//     }
+// })
+
